32f06b3e016f7596c83ab883d2f666b6c8726a86
Clément Robert <cr52@protonmail.com>
2023-09-24 10:42:58 +0200

    cleanup

--- t.py
+++ t.py
@@ -3,23 +3,21 @@ from yt.utilities.lib.misc_utilities import obtain_relative_velocity_vector
 from min_yt import Dataset, FieldInfoContainer
 
 
-def setup_fluid_fields(registry):
+def foo():
+    ds = Dataset()
+    ds.field_info = FieldInfoContainer(ds)
+    ds.coordinates.setup_fields(ds.field_info)
+
     def create_vector_fields(registry) -> None:
         def foo_closure(field, data):
             obtain_relative_velocity_vector(data, (xn, yn, zn), "bulk_velocity")
 
         xn, yn, zn = (("gas", f"velocity_{ax}") for ax in "xyz")
 
-        registry.add_field(("gas", "velocity_spherical_radius"), function=foo_closure)
+        ds.field_info.add_field(("gas", "velocity_spherical_radius"), function=foo_closure)
 
-    create_vector_fields(registry)
+    create_vector_fields(ds.field_info)
 
-
-def foo():
-    ds = Dataset()
-    ds.field_info = FieldInfoContainer(ds)
-    ds.coordinates.setup_fields(ds.field_info)
-    setup_fluid_fields(ds.field_info)
     ds.field_info.check_derived_fields()
 
 
