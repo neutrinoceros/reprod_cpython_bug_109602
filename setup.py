2ffe56a8384507b01edba1c71f22c159ffba6718
Clément Robert <cr52@protonmail.com>
2023-09-24 11:50:05 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.extension import Extension
 
-import numpy
 from Cython.Build import cythonize
 from setuptools import setup
 
@@ -11,11 +10,6 @@ setup(
             Extension(
                 "mini_yt.lib",
                 sources=["src/mini_yt/lib.pyx"],
-                include_dirs=[numpy.get_include()],
-                define_macros=[
-                    ("NPY_TARGET_VERSION", "NPY_1_21_API_VERSION"),
-                    ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"),
-                ],
             )
         ],
         compiler_directives={"language_level": 3},
