604f0ccf8559ee59115c86b6eb16ef2ab54aa6ed
Clément Robert <cr52@protonmail.com>
2023-09-24 16:25:59 +0200

    cleanup

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ from mini_yt import Coordinates, Dataset
 from mini_yt.lib import bar
 
 
-def foo():
+def main():
     ds = Dataset()
     coordinates = Coordinates()
     coordinates.setup_fields(ds.field_info)
@@ -16,5 +16,5 @@ def foo():
 
 NLOOPS = 3000
 for i in range(1, NLOOPS + 1):
-    foo()
+    main()
     print(f"{i}/{NLOOPS}", end="\r")
