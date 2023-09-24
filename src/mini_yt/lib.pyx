1f2bb3287237fbef5c0c17fea196f0299f3c3fe6
Clément Robert <cr52@protonmail.com>
2023-09-24 11:40:33 +0200

    cleanup

--- src/mini_yt/lib.pyx
+++ src/mini_yt/lib.pyx
@@ -13,18 +13,5 @@ def obtain_relative_velocity_vector(
         field_names = (("gas", "velocity_x"), ("gas", "velocity_y"), ("gas", "velocity_z")),
         bulk_vector = "bulk_velocity"
     ):
-    # This is just to let the pointers exist and whatnot.  We can't cdef them
-    # inside conditionals.
-    cdef np.ndarray[np.float64_t, ndim=1] vxf
-    cdef np.ndarray[np.float64_t, ndim=1] vyf
-    cdef np.ndarray[np.float64_t, ndim=1] vzf
-    cdef np.ndarray[np.float64_t, ndim=2] rvf
-    cdef np.ndarray[np.float64_t, ndim=3] vxg
-    cdef np.ndarray[np.float64_t, ndim=3] vyg
-    cdef np.ndarray[np.float64_t, ndim=3] vzg
-    cdef np.ndarray[np.float64_t, ndim=4] rvg
-    cdef np.float64_t bv[3]
-    cdef int i, j, k, dim
-
     units = data[field_names[0]].units
     raise AttributeError
