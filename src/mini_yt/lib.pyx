04480e2978b698071a892311e2dccbff94f6b844
Clément Robert <cr52@protonmail.com>
2023-09-24 11:45:13 +0200

    cleanup

--- src/mini_yt/lib.pyx
+++ src/mini_yt/lib.pyx
@@ -2,7 +2,7 @@
 
 def obtain_relative_velocity_vector(
         data,
-        field_names = (("gas", "velocity_x"), ("gas", "velocity_y"), ("gas", "velocity_z")),
+        field_names = (("gas", "velocity_x"),),
         bulk_vector = "bulk_velocity"
     ):
     units = data[field_names[0]].units
